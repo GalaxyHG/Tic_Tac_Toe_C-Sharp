ce77a9c0afed3656443a0aa289bc082111aca67b
Helena Rezende <99421906+GalaxyHG@users.noreply.github.com>
2023-06-12 11:22:11 -0300

    Add files via upload

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblplayerone = new System.Windows.Forms.Label();
+            this.lblplayertwo = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.lblPlayerTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblplayerone
+            // 
+            this.lblplayerone.AutoSize = true;
+            this.lblplayerone.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblplayerone.ForeColor = System.Drawing.Color.ForestGreen;
+            this.lblplayerone.Location = new System.Drawing.Point(13, 13);
+            this.lblplayerone.Name = "lblplayerone";
+            this.lblplayerone.Size = new System.Drawing.Size(100, 29);
+            this.lblplayerone.TabIndex = 0;
+            this.lblplayerone.Text = "Player 1:";
+            // 
+            // lblplayertwo
+            // 
+            this.lblplayertwo.AutoSize = true;
+            this.lblplayertwo.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblplayertwo.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lblplayertwo.Location = new System.Drawing.Point(207, 13);
+            this.lblplayertwo.Name = "lblplayertwo";
+            this.lblplayertwo.Size = new System.Drawing.Size(100, 29);
+            this.lblplayertwo.TabIndex = 1;
+            this.lblplayertwo.Text = "Player 2:";
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(13, 52);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(100, 100);
+            this.btn1.TabIndex = 2;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(119, 52);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(100, 100);
+            this.btn2.TabIndex = 3;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(225, 52);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(100, 100);
+            this.btn3.TabIndex = 4;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(13, 158);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(100, 100);
+            this.btn4.TabIndex = 5;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(119, 158);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(100, 100);
+            this.btn5.TabIndex = 6;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(225, 158);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(100, 100);
+            this.btn6.TabIndex = 7;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn7.CausesValidation = false;
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(13, 264);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(100, 100);
+            this.btn7.TabIndex = 8;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(119, 264);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(100, 100);
+            this.btn8.TabIndex = 9;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(225, 264);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(100, 100);
+            this.btn9.TabIndex = 10;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // lblPlayerTime
+            // 
+            this.lblPlayerTime.AutoSize = true;
+            this.lblPlayerTime.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerTime.ForeColor = System.Drawing.Color.Red;
+            this.lblPlayerTime.Location = new System.Drawing.Point(13, 367);
+            this.lblPlayerTime.Name = "lblPlayerTime";
+            this.lblPlayerTime.Size = new System.Drawing.Size(181, 26);
+            this.lblPlayerTime.TabIndex = 11;
+            this.lblPlayerTime.Text = "Vez de: Player 1 (X)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(339, 397);
+            this.Controls.Add(this.lblPlayerTime);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblplayertwo);
+            this.Controls.Add(this.lblplayerone);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblplayerone;
+        private System.Windows.Forms.Label lblplayertwo;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Label lblPlayerTime;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int playeronewins = 0, playertwowins = 0, jogadas = 0;
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (jogadas % 2 == 0)
+            {
+                btn2.Text = "X";
+                lblPlayerTime.Text = "Vez de: Player 2 (O).";
+                lblPlayerTime.ForeColor = Color.LimeGreen;
+            }
+            else
+            {
+                btn2.Text = "O";
+                lblPlayerTime.Text = "Vez de: Player 1 (X).";
+                lblPlayerTime.ForeColor = Color.Red;
+            }
+            jogadas++;
+
+            btn2.Enabled = false;
+            Testar();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (jogadas % 2 == 0)
+            {
+                btn3.Text = "X";
+                lblPlayerTime.Text = "Vez de: Player 2 (O).";
+                lblPlayerTime.ForeColor = Color.LimeGreen;
+            }
+            else
+            {
+                btn3.Text = "O";
+                lblPlayerTime.Text = "Vez de: Player 1 (X).";
+                lblPlayerTime.ForeColor = Color.Red;
+            }
+            jogadas++;
+
+            btn3.Enabled = false;
+            Testar();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (jogadas % 2 == 0)
+            {
+                btn4.Text = "X";
+                lblPlayerTime.Text = "Vez de: Player 2 (O).";
+                lblPlayerTime.ForeColor = Color.LimeGreen;
+            }
+            else
+            {
+                btn4.Text = "O";
+                lblPlayerTime.Text = "Vez de: Player 1 (X).";
+                lblPlayerTime.ForeColor = Color.Red;
+            }
+            jogadas++;
+
+            btn4.Enabled = false;
+            Testar();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (jogadas % 2 == 0)
+            {
+                btn5.Text = "X";
+                lblPlayerTime.Text = "Vez de: Player 2 (O).";
+                lblPlayerTime.ForeColor = Color.LimeGreen;
+            }
+            else
+            {
+                btn5.Text = "O";
+                lblPlayerTime.Text = "Vez de: Player 1 (X).";
+                lblPlayerTime.ForeColor = Color.Red;
+            }
+            jogadas++;
+
+            btn5.Enabled = false;
+            Testar();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (jogadas % 2 == 0)
+            {
+                btn6.Text = "X";
+                lblPlayerTime.Text = "Vez de: Player 2 (O).";
+                lblPlayerTime.ForeColor = Color.LimeGreen;
+            }
+            else
+            {
+                btn6.Text = "O";
+                lblPlayerTime.Text = "Vez de: Player 1 (X).";
+                lblPlayerTime.ForeColor = Color.Red;
+            }
+            jogadas++;
+
+            btn6.Enabled = false;
+            Testar();
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (jogadas % 2 == 0)
+            {
+                btn7.Text = "X";
+                lblPlayerTime.Text = "Vez de: Player 2 (O).";
+                lblPlayerTime.ForeColor = Color.LimeGreen;
+            }
+            else
+            {
+                btn7.Text = "O";
+                lblPlayerTime.Text = "Vez de: Player 1 (X).";
+                lblPlayerTime.ForeColor = Color.Red;
+            }
+            jogadas++;
+
+            btn7.Enabled = false;
+            Testar();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (jogadas % 2 == 0)
+            {
+                btn8.Text = "X";
+                lblPlayerTime.Text = "Vez de: Player 2 (O).";
+                lblPlayerTime.ForeColor = Color.LimeGreen;
+            }
+            else
+            {
+                btn8.Text = "O";
+                lblPlayerTime.Text = "Vez de: Player 1 (X).";
+                lblPlayerTime.ForeColor = Color.Red;
+            }
+            jogadas++;
+
+            btn8.Enabled = false;
+            Testar();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (jogadas % 2 == 0)
+            {
+                btn9.Text = "X";
+                lblPlayerTime.Text = "Vez de: Player 2 (O).";
+                lblPlayerTime.ForeColor = Color.LimeGreen;
+            }
+            else
+            {
+                btn9.Text = "O";
+                lblPlayerTime.Text = "Vez de: Player 1 (X).";
+                lblPlayerTime.ForeColor = Color.Red;
+            }
+            jogadas++;
+
+            btn9.Enabled = false;
+            Testar();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (jogadas % 2 == 0)
+            {
+                btn1.Text = "X";
+                lblPlayerTime.Text = "Vez de: Player 2 (O).";
+                lblPlayerTime.ForeColor = Color.LimeGreen;
+            }
+            else
+            {
+                btn1.Text = "O";
+                lblPlayerTime.Text = "Vez de: Player 1 (X).";
+                lblPlayerTime.ForeColor = Color.Red;
+            }
+            jogadas++;
+
+            btn1.Enabled = false;
+            Testar();
+        }
+
+        public void Testar()
+        {
+            if (btn1.Text == "X" && btn2.Text == "X" && btn3.Text == "X")
+            {
+                MessageBox.Show("Player 1 GANHOU!");
+                playeronewins++;
+                lblplayerone.Text = "Player 1: " + playeronewins;
+                Limpar();
+            }
+            else if (btn1.Text == "X" && btn4.Text == "X" && btn7.Text == "X")
+            {
+                MessageBox.Show("Player 1 GANHOU!");
+                playeronewins++;
+                lblplayerone.Text = "Player 1: " + playeronewins;
+                Limpar();
+            }
+            else if (btn1.Text == "X" && btn5.Text == "X" && btn9.Text == "X")
+            {
+                MessageBox.Show("Player 1 GANHOU!");
+                playeronewins++;
+                lblplayerone.Text = "Player 1: " + playeronewins;
+                Limpar();
+            }
+            else if (btn3.Text == "X" && btn5.Text == "X" && btn7.Text == "X")
+            {
+                MessageBox.Show("Player 1 GANHOU!");
+                playeronewins++;
+                lblplayerone.Text = "Player 1: " + playeronewins;
+                Limpar();
+            }
+            else if (btn7.Text == "X" && btn8.Text == "X" && btn9.Text == "X")
+            {
+                MessageBox.Show("Player 1 GANHOU!");
+                playeronewins++;
+                lblplayerone.Text = "Player 1: " + playeronewins;
+                Limpar();
+            }
+            else if (btn3.Text == "X" && btn6.Text == "X" && btn9.Text == "X")
+            {
+                MessageBox.Show("Player 1 GANHOU!");
+                playeronewins++;
+                lblplayerone.Text = "Player 1: " + playeronewins;
+                Limpar();
+            }
+            else if (btn4.Text == "X" && btn5.Text == "X" && btn6.Text == "X")
+            {
+                MessageBox.Show("Player 1 GANHOU!");
+                playeronewins++;
+                lblplayerone.Text = "Player 1: " + playeronewins;
+                Limpar();
+            }
+            else if (btn2.Text == "X" && btn5.Text == "X" && btn8.Text == "X")
+            {
+                MessageBox.Show("Player 1 GANHOU!");
+                playeronewins++;
+                lblplayerone.Text = "Player 1: " + playeronewins;
+                Limpar();
+            }
+            else if (btn1.Text == "O" && btn2.Text == "O" && btn3.Text == "O")
+            {
+                MessageBox.Show("Player 2 GANHOU!");
+                playertwowins++;
+                lblplayertwo.Text = "Player 2: " + playertwowins;
+                Limpar();
+            }
+            else if (btn1.Text == "O" && btn4.Text == "O" && btn7.Text == "O")
+            {
+                MessageBox.Show("Player 2 GANHOU!");
+                playertwowins++;
+                lblplayertwo.Text = "Player 2: " + playertwowins;
+                Limpar();
+            }
+            else if (btn1.Text == "O" && btn5.Text == "O" && btn9.Text == "O")
+            {
+                MessageBox.Show("Player 2 GANHOU!");
+                playertwowins++;
+                lblplayertwo.Text = "Player 2: " + playertwowins;
+                Limpar();
+            }
+            else if (btn3.Text == "O" && btn5.Text == "O" && btn7.Text == "O")
+            {
+                MessageBox.Show("Player 2 GANHOU!");
+                playertwowins++;
+                lblplayertwo.Text = "Player 2: " + playertwowins;
+                Limpar();
+            }
+            else if (btn7.Text == "O" && btn8.Text == "O" && btn9.Text == "O")
+            {
+                MessageBox.Show("Player 2 GANHOU!");
+                playertwowins++;
+                lblplayertwo.Text = "Player 2: " + playertwowins;
+                Limpar();
+            }
+            else if (btn3.Text == "O" && btn6.Text == "O" && btn9.Text == "O")
+            {
+                MessageBox.Show("Player 2 GANHOU!");
+                playertwowins++;
+                lblplayertwo.Text = "Player 2: " + playertwowins;
+                Limpar();
+            }
+            else if (btn4.Text == "O" && btn5.Text == "O" && btn6.Text == "O")
+            {
+                MessageBox.Show("Player 2 GANHOU!");
+                playertwowins++;
+                lblplayertwo.Text = "Player 2: " + playertwowins;
+                Limpar();
+            }
+            else if (btn2.Text == "O" && btn5.Text == "O" && btn8.Text == "O")
+            {
+                MessageBox.Show("Player 2 GANHOU!");
+                playertwowins++;
+                lblplayertwo.Text = "Player 2: " + playertwowins;
+                Limpar();
+            }
+            else if (btn1.Text != "" && btn2.Text != "" && btn3.Text != "" && btn4.Text != "" && btn5.Text != "" && btn6.Text != "" && btn7.Text != "" && btn8.Text != "" && btn9.Text != "")
+            {
+                MessageBox.Show("Deu velha!");
+                Limpar();
+            }
+        }
+
+        public void Limpar()
+        {
+            btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+            btn4.Enabled = true;
+            btn5.Enabled = true;
+            btn6.Enabled = true;
+            btn7.Enabled = true;
+            btn8.Enabled = true;
+            btn9.Enabled = true;
+
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+
+            jogadas = 0;
+
+            lblPlayerTime.Text = "Vez de: Player 1 (X).";
+            lblPlayerTime.ForeColor = Color.Red;
+        }
+    }
+}
